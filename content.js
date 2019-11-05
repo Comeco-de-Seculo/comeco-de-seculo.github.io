15b0ecc38e5d53e459221a9918c271491bb89a37
Diogo V. Araújo <diogovaraujo@gmail.com>
2019-11-04 21:09:59 -0300

    Updated page title

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 const content = ({
+  title: 'titulo da pagina',
   header: {
       text1: 'Começo de Século (Beginning of a Century) is a live apotheotic epic — part exhibition, happening and experimental theater — that happens in one-act on November 5, from midnight to three in the morning, at Galeria Jaqueline Martins, and',
       text2: 'is accompanied by this site with a selection of commissioned videos, and activities in public spaces throughout this end of the year.',
@@ -10,6 +11,11 @@ const content = ({
       index: 'CDS01',
       length: '4:03"',
       title: 'CRISE por COMEÇO DE SÉCULO VT'
+    },
+    {
+      index: 'CDS02',
+      length: '4:12"',
+      title: 'CRISE por COMEÇO DE SÉCULO VVMTII'
     }
   ]
 })
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import content from '../content'
 
 function Home() {
     return (
       <div className='page'>
+        
+        <Head>
+          <link rel="stylesheet" type="text/css" href="/static/fonts/stylesheet.css" />
+          <title>{content.title}</title>
+        </Head>
         <div>
           <div><img src='/static/logo.png' /></div>
           <div>{content.header.text1}</div>
@@ -11,13 +17,15 @@ function Home() {
           <div>{content.header.text4}</div>
         </div>
         <table>
-          {content.list.map(item =>
-            <tr>
-              <td>{item.index}</td>
-              <td>{item.length}</td>
-              <td>{item.title}</td>
-            </tr>
-          )}
+          <tbody>
+            {content.list.map(item =>
+              <tr key={item.index}>
+                <td>{item.index}</td>
+                <td>{item.length}</td>
+                <td>{item.title}</td>
+              </tr>
+            )}
+          </tbody>
         </table>
         <style jsx global>{`
           body {
@@ -33,6 +41,8 @@ function Home() {
             color: #8230FA;
             flex-direction: column;
             font-size: 20px;
+            font-family: 'Spectral';
+            font-weight: 300;
           }
           .page > * {
             padding: 30px;
