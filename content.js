50ff07deafea5a686c7f1227242d6307c68f0153
Diogo V. Araújo <diogovaraujo@gmail.com>
2019-11-04 22:05:02 -0300

    newline example

--- content.js
+++ content.js
@@ -3,8 +3,8 @@ const content = ({
   header: {
       text1: 'Começo de Século (Beginning of a Century) is a live apotheotic epic — part exhibition, happening and experimental theater — that happens in one-act on November 5, from midnight to three in the morning, at Galeria Jaqueline Martins, and',
       text2: 'is accompanied by this site with a selection of commissioned videos, and activities in public spaces throughout this end of the year.',
-      text3: 'Curated by: Germano Dushá Curatorial assistant: Guilherme Teixeira',
-      text4: 'São Paulo 2019'
+      text3: 'Curated by:\nGermano Dushá\nCuratorial assistant:\nGuilherme Teixeira',
+      text4: 'São\nPaulo\n2019'
   },
   list: [
     {
--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,16 @@ function Home() {
           <title>{content.title}</title>
         </Head>
         <div>
-          <div><img src='/static/logo.png' /></div>
-          <div>{content.header.text1}</div>
-          <div>{content.header.text2}</div>
-          <div>{content.header.text3}</div>
-          <div>{content.header.text4}</div>
+          <img src='/static/logo.png' />
+          <div className='t1'>{content.header.text1}</div>
+          <div className='t2'>{content.header.text2}</div>
+          <div className='t3'>{content.header.text3}</div>
+          <div className='t4'>{content.header.text4}</div>
         </div>
         <table>
           <tbody>
             {content.list.map(item =>
-              <tr key={item.index}>
+              <tr key={item.index} className='scale-text'>
                 <td>{item.index}</td>
                 <td>{item.length}</td>
                 <td>{item.title}</td>
@@ -40,9 +40,10 @@ function Home() {
             height: 100vh;
             color: #8230FA;
             flex-direction: column;
-            font-size: 20px;
+            font-size: 16px;
             font-family: 'Spectral';
             font-weight: 300;
+            white-space: pre-wrap;
           }
           .page > * {
             padding: 30px;
@@ -57,6 +58,33 @@ function Home() {
             height: 140px;
           }
           
+          .page > div:first-child {
+            border-bottom: 1px solid #8230FA;
+            height: 140px;
+            display: flex;
+            justify-content: space-between;
+          }
+          .page > div:first-child > img {
+            height: 140px;
+          }
+          .page > div:first-child > div {
+            max-width: 340px;
+            transform: scaleY(2) translateY(22%);
+            line-height: 1.2em;
+          }
+          .page > div:first-child > div.t4 {
+            transform: scaleY(2) translateY(26%);
+            line-height: 1em;
+            font-size: 20px;
+          }
+          .page > table {
+          }
+          .page > table tr {
+            font-size: 30px;
+            font-weight: 200;
+            line-height: 2em;
+            transform: scaleY(2);
+          }
         `}</style>
       </div>
   )}
