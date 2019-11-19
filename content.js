07943884f5762177ab3e53680324c0705e52b113
Germano Dushá <germanodsantos@gmail.com>
2019-11-19 15:38:47 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const content = ({
   title: 'Começo de Século',
   header: {
-      text1: 'Começo de Século (Beginning of a Century) is a live apotheotic epic — part exhibition, happening and experimental theater — that happens in one-act on November 5, from midnight to three in the morning',
+      text1: 'Beginning of a Century is a live apotheotic epic — part exhibition, happening and experimental theater — that happens in one-act on Nov 5, from 00h00 to 3 a.m.',
       text2: 'at Galeria Jaqueline Martins, and is followed by unannounced activities in public spaces throughout the end of the year, and this site with commissioned and documentation videos.',
       text3: 'Curated by:\nGermano Dushá\nCuratorial assistant:\nGuilherme Teixeira',
       text4: 'São\nPaulo\n2019'
